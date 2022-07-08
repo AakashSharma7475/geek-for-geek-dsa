b9d0e7e575caee33b2592f98a7219aa8adb6a166
AakashSharma7475 <99554677+AakashSharma7475@users.noreply.github.com>
2022-07-08 11:36:24 +0530

    Create check kth bit is set or not.cpp

---
+++ check kth bit is set or not.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    
+    (x>>y-1);
+    if(x&1!=0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+    return 0;
+}
