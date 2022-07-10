1c38a993f3fee207a60e09f9abda56c1fc871f06
AakashSharma7475 <99554677+AakashSharma7475@users.noreply.github.com>
2022-07-10 20:40:40 +0530

    Update look up table method for count set bits.cpp

--- look up table method for count set bits.cpp
+++ look up table method for count set bits.cpp
@@ -1,1 +1,37 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int table[256];
+
+void initialization()
+{
+    table[0]=0;
+    for(int i=0;i<256;i++)
+    {
+        table[i]=(i&1)+table[i/2];
+    }
+}
+
+int main()
+{
+    initialization();
+    int n,res;
+    cin>>n;
+    
+    res=table[n&0xff];
+    n=n>>8;
+    res=res+table[n&0xff];
+    n=n>>8;
+    res=res+table[n&0xff];
+    n=n>>8;
+    res=res+table[n&0xff];
+    
+    cout<<res;
+    
+    
+    
+    return 0;
+}
+
