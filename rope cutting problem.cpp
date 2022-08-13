db5d2e41e232b924d8eea6bddc71dbd8974fc829
AakashSharma7475 <99554677+AakashSharma7475@users.noreply.github.com>
2022-08-14 01:13:25 +0530

    Create rope cutting problem.cpp

---
+++ rope cutting problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+// #include <algorithm>
+
+using namespace std;
+
+int rope(int n,int a,int b,int c)
+{
+    if(n==0)
+    return 0;
+    if(n<=-1)
+    return -1;
+    
+    int res=max(rope(n-a,a,b,c),
+    max(rope(n-b,a,b,c),
+    rope(n-c,a,b,c)));
+    
+    if(res==-1)
+    return -1;
+    
+    return res+1;
+}
+int main()
+{   
+    int n,a,b,c;
+    cin>>n>>a>>b>>c;
+    cout<<rope(n,a,b,c);
+
+    return 0;
+}
