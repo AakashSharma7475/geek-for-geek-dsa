33b0214bd4e9d4322add9ac70319115546d283f0
AakashSharma7475 <99554677+AakashSharma7475@users.noreply.github.com>
2022-10-14 13:10:35 +0530

    Update (49)union of two sorted arrays with merge function.cpp

--- (49)union of two sorted arrays with merge function.cpp
+++ (49)union of two sorted arrays with merge function.cpp
@@ -4,17 +4,25 @@ void union_of_two_sorted_arrays(int arr1[],int arr2[],int n1,int n2)
 {
     int i=0,j=0;
     while(i<n1&&j<n2)
-    {
-        if(i>0&&arr1[i]==arr1[i-1])
+    {        
+        if(i>0&&arr1[i]==arr1[i-1])    
         {
+            i++;
+            continue;
+        }
+        if(j>0&&arr2[j]==arr2[j-1])
+        {
+            j++;
             continue;
         }
         if(arr1[i]<arr2[j])
         {
+            cout<<arr1[i]<<" ";
             i++;
         }
         else if(arr1[i]>arr2[j])
         {
+            cout<<arr2[j]<<" ";
             j++;
         }
         else
@@ -23,6 +31,18 @@ void union_of_two_sorted_arrays(int arr1[],int arr2[],int n1,int n2)
             i++;
             j++;
         }
+    }
+    while(i<n1)
+    {
+        if(i==0||arr1[i]!=arr1[i-1])
+        cout<<arr1[i]<<" ";
+        i++;
+    }
+     while(j<n2)
+    {
+        if(j==0||arr2[j]!=arr2[j-1])
+        cout<<arr2[j]<<" ";
+        j++;
     }
 }
 
@@ -35,9 +55,9 @@ int main()
     {
         cin>>arr1[i];
     }
-      cin>>n2;
+    cin>>n2;
     int arr2[n2];
-    for(int i=0;i<n1;i++)
+    for(int i=0;i<n2;i++)
     {
         cin>>arr2[i];
     }
